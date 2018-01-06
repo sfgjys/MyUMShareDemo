a2f4e497e53a717efddb739b28668694679785bc
ZHQ <sfgjys@sina.com>
2018-01-06 17:14:31 +0800

    细节

--- app/src/main/java/com/umeng/soexample/CommonApplication.java
+++ app/src/main/java/com/umeng/soexample/CommonApplication.java
@@ -24,18 +24,33 @@ public class CommonApplication extends Application {
 
     //各个平台的配置 配置三方平台的appkey：，建议放在全局Application或者程序入口
     static {
+        PlatformConfig.setAlipay("2015111700822536");
+        PlatformConfig.setDing("dingoalmlnohc0wggfedpk");
+        // 豆瓣RENREN平台目前只能在服务器端配置
+        PlatformConfig.setDropbox("oz8v5apet3arcdy", "h7p2pjbzkkxt02a");
+        // 邮件
+        // 印象笔记
+        // facebook
+        // flickr
+        // foursquare
+        // gplus
+        // instagram
+        PlatformConfig.setKakao("e4f60e065048eb031e235c806b31c70f");
+        // line
+        // linkedin
+        PlatformConfig.setPinterest("1439206");
+        // pocket
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+        // renren
+        // 短信
+        // 腾讯微博
+        // tumblr
+        PlatformConfig.setTwitter("3aIN7fuF685MuZ7jtXkQxalyi", "MK6FEYG63eWcpDFgRYw4w9puJhzDl0tyuqWjZ3M7XJuuG7mMbO");
+        PlatformConfig.setVKontakte("5764965", "5My6SNliAaLxEm3Lyd9J");
         PlatformConfig.setWeixin("wxdc1e388c3822c80b", "3baf1193c85774b3fd9d18447d76cab0");
-        //豆瓣RENREN平台目前只能在服务器端配置
+        // whatsapp
         PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad", "http://sns.whalecloud.com");
         PlatformConfig.setYixin("yxc0614e80c9304c11b0391514d09f13bf");
-        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
-        PlatformConfig.setTwitter("3aIN7fuF685MuZ7jtXkQxalyi", "MK6FEYG63eWcpDFgRYw4w9puJhzDl0tyuqWjZ3M7XJuuG7mMbO");
-        PlatformConfig.setAlipay("2015111700822536");
-        PlatformConfig.setLaiwang("laiwangd497e70d4", "d497e70d4c3e4efeab1381476bac4c5e");
-        PlatformConfig.setPinterest("1439206");
-        PlatformConfig.setKakao("e4f60e065048eb031e235c806b31c70f");
-        PlatformConfig.setDing("dingoalmlnohc0wggfedpk");
-        PlatformConfig.setVKontakte("5764965", "5My6SNliAaLxEm3Lyd9J");
-        PlatformConfig.setDropbox("oz8v5apet3arcdy", "h7p2pjbzkkxt02a");
+        // ynote
     }
 }
