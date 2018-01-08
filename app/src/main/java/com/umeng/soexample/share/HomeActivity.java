6d3ac0d08d3145e9fb427cd9f8f68d0d69f5a223
ZHQ <sfgjys@sina.com>
2018-01-08 16:48:09 +0800

    准备测试友盟分享功能模块化

--- app/src/main/java/com/umeng/soexample/share/HomeActivity.java
+++ app/src/main/java/com/umeng/soexample/share/HomeActivity.java
@@ -10,6 +10,6 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.zhq_home_main_share);
     }
 }
